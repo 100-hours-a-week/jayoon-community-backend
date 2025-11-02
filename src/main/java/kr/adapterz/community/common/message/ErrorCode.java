7508e12861b9346a038ab19e201ada4cd50e3174
brixxt27 <brixxt27@gmail.com>
2025-11-02 22:54:51 +0900

    feat: JwtManager 구현
    - Refresh token는 초기 모델에서 서버에서 메모리로 관리될 예정이므로 단순 UUID로 구현했습니다. 이후 Redis나 DB로 저장소를 옮길 에정입니다.

--- src/main/java/kr/adapterz/community/common/message/ErrorCode.java
+++ src/main/java/kr/adapterz/community/common/message/ErrorCode.java
@@ -23,11 +23,6 @@ public enum ErrorCode {
     INPUT_VALUE_INVALID("입력값이 올바르지 않습니다."),
     INTERNAL_ERROR_SERVER("서버에 문제가 발생했습니다. 관리자에게 문의해주세요."),
 
-    // Authentication
-    AUTH_FAILURE("이메일 또는 비밀번호가 잘못되었습니다."),
-    TOKEN_EXPIRED("인증 토큰이 만료되었습니다."),
-    TOKEN_INVALID("인증 토큰이 유효하지 않습니다."),
-
     // User
     USER_NOT_FOUND("해당 사용자를 찾을 수 없습니다."),
     USER_EMAIL_BLANK("이메일은 필수 입력 값입니다."),
@@ -35,9 +30,14 @@ public enum ErrorCode {
     USER_NICKNAME_ALREADY_EXISTED("닉네임이 이미 존재합니다."),
     USER_PASSWORD_MISMATCH("비밀번호가 일치하지 않습니다."),
 
+    // Authentication
+    AUTH_FAILURE("이메일 또는 비밀번호가 잘못되었습니다."),
+    TOKEN_EXPIRED("인증 토큰이 만료되었습니다."),
+    TOKEN_INVALID("인증 토큰이 유효하지 않습니다."),
+    INVALID_SESSION("유효하지 않는 세션 정보입니다."),
+
     // UserAuth
     USER_AUTH_NOT_FOUND("유저 인증 정보가 존재하지 않습니다."),
-    INVALID_SESSION("유효하지 않는 세션 정보입니다."),
 
     // Image
     INVALID_IMAGE_PATH("이미지의 경로가 올바르지 않습니다."),
--- src/main/java/kr/adapterz/community/common/security/jwt/JwtManager.java
+++ src/main/java/kr/adapterz/community/common/security/jwt/JwtManager.java
@@ -1,16 +1,132 @@
 package kr.adapterz.community.common.security.jwt;
 
+import static kr.adapterz.community.common.message.ErrorCode.TOKEN_EXPIRED;
+import static kr.adapterz.community.common.message.ErrorCode.TOKEN_INVALID;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import java.util.Date;
+import java.util.UUID;
+import javax.crypto.SecretKey;
+import kr.adapterz.community.common.exception.dto.UnauthorizedException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtManager {
-    // ToDo 인증 정보 생성, 7주차 구현 예정
-    public JwtDto generateToken(Long userId) {
-        return JwtDto.builder()
-                .tokenType("Bearer")
-                .accessToken("example")
-                .refreshToken("example")
-                .expiresIn(3600L)
-                .build();
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.access-token-expiration-ms}")
+    private long accessTokenExpirationMs;
+
+    @Value("${jwt.refresh-token-expiration-ms}")
+    private long refreshTokenExpirationMs;
+
+    private SecretKey key;
+
+    /**
+     * Secret key로 사용할 key 필드를 할당합니다.
+     * <p>
+     * '@Value' 주입 시기는 생성자 호출 시기 보다 빠르므로 '@PostConstruct'에서 처리합니다.
+     */
+    @PostConstruct
+    public void init() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    /**
+     * Access token과 Refresh token을 갖고 있는 DTO를 생성합니다.
+     *
+     * @param userId
+     * @return
+     */
+    public JwtDto generateTokens(Long userId) {
+        String accessToken = generateAccessToken(userId);
+        String refreshToken = generateRefreshToken();
+        return new JwtDto(accessToken, refreshToken);
+    }
+
+    /**
+     * Access token을 생성합니다. payload에 userId를 포함합니다.
+     *
+     * @param userId
+     * @return
+     */
+    public String generateAccessToken(Long userId) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + accessTokenExpirationMs);
+
+        return Jwts.builder()
+                .subject(String.valueOf(userId))
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(key)
+                .compact();
+    }
+
+    /**
+     * Refresh token을 생성합니다. 세션처럼 관리만 하면 되기 때문에 단순 문자열인 UUID를 사용합니다.
+     * <p>
+     * 이후 실제 JWT로 발급하여 관리할지, DB에서 관리할지, Redis에서 관리할지 고민입니다.
+     * <p>
+     * JWT로 관리하면 만료일자를 서버에서 직접 관리하지 않아도 되는 장점이 있습니다.
+     * <p>
+     * UUID로 관리하면 refresh token에 페이로드가 없기 때문에 단순하게 관리할 수 있고 유효성 검사가 비교적 간단하지만, 만료 일자를 쿠키에 넣어도 웹
+     * 브라우저에서 이를 변경 할 수 있기 때문에 안전하지 않습니다.
+     *
+     * @return UUID
+     */
+    public String generateRefreshToken() {
+        return UUID.randomUUID().toString();
+    }
+
+    /**
+     * 토큰의 유효성 검사를 진행합니다.
+     *
+     * @param token
+     * @return
+     * @Exception IllegalArgumentException은 parseSignedClaims에서 token이 null이나 비어있거나 white space 하나만
+     * 있을 때 발생합니다.
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            throw new UnauthorizedException(TOKEN_EXPIRED);
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new UnauthorizedException(TOKEN_INVALID);
+        }
+    }
+
+    /**
+     * Access token에서 userId를 추출합니다.
+     *
+     * @param token
+     * @return
+     * @Exception IllegalArgumentException은 parseSignedClaims에서 token이 null이나 비어있거나 white space 하나만
+     * 있을 때 발생합니다.
+     */
+    public Long getUserIdFromToken(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+            return Long.parseLong(claims.getSubject());
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new UnauthorizedException(TOKEN_INVALID);
+        }
     }
 }
