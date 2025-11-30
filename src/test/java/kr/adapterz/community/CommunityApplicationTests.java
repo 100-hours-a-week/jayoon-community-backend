2286cc19a42fbfd4b5ecfc1c574206d7243e6004
brixxt27 <brixxt27@gmail.com>
2025-12-01 01:11:15 +0900

    chore: test 추가
    1. 테스트 시 기본적으로 통합 테스트를 진행하는데 이 의존성을 맞춰주려면 기본적으로 Bean을 생성할 때 필요한 값들을 추가해줘야 한다.
    2. github actions에서 메모리 기반의 통합테스트를 진행하기 위해 H2 테스트 구현체를 추가했다.
    3. 빈 생성을 프로파일에 따라 선택하므로 테스트에 명시했다.
    4. 이미지 빌드 및 푸시는 CI가 성공해야만 가능하다.

--- src/test/java/kr/adapterz/community/CommunityApplicationTests.java
+++ src/test/java/kr/adapterz/community/CommunityApplicationTests.java
@@ -2,8 +2,10 @@ package kr.adapterz.community;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("jwt")
 class CommunityApplicationTests {
 
     @Test
